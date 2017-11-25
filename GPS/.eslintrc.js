a0febe34adf1be4ab72056661d83dd987bbc73b9
Sendhav <sendhav05@gmail.com>
2017-11-25 22:13:19 +0530

    Create eslint config file

---
+++ GPS/.eslintrc.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": ["error", { "extensions": [".js"] }],
+      "import/prefer-default-export": 0,
+      "react/no-string-refs": 0,
+      "class-methods-use-this": 0,
+      "no-unused-expressions": 0,
+      "react/no-unused-prop-types": 0
+    }
+   };
+   
