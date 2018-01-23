93d826ca3930875cae13130cdfdb4ad548883d63
Mac <sendhav05@gmail.com>
2018-01-24 03:40:42 +0530

    Android notification

--- GPS/android/app/src/main/java/com/gps/MainApplication.java
+++ GPS/android/app/src/main/java/com/gps/MainApplication.java
@@ -3,6 +3,7 @@ package com.gps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
      protected List<ReactPackage> getPackages() {
          return Arrays.<ReactPackage>asList(
                  new MainReactPackage(),
+                 new RNNotificationsPackage(MainApplication.this),
                  new MapsPackage()
          );
      }
